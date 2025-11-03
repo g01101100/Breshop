f96b5865ecf4d6ba8752f3d0f026fa0f41cdca3c
Guilherme Lira <guilherme.liratsouza@gmail.com>
2025-11-02 21:49:50 -0300

    feat: modelos criados e migrates

--- breshop/admin.py
+++ breshop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import *
+
+admin.site.register(Adress)
+
+admin.site.register(Brecho)
+
+admin.site.register(Contact)
+
+admin.site.register(Tag)
+
+admin.site.register(Produto)
+
+admin.site.register(User)
 # Register your models here.
--- breshop/models.py
+++ breshop/models.py
@@ -1,22 +1,40 @@
 from django.db import models
 
 class Adress(models.Model):
-    CEP = models.CharField(max_length = 8)
+    CEP = models.CharField(max_length=8)
     state = models.CharField(max_length=20)
     city = models.CharField(max_length=20)
     street = models.CharField(max_length=50)
     number = models.IntegerField()
 	
-    def __str__(self) -> str:
-        return self.CEP
-    
+
+class Brecho(models.Model):
+    name = models.CharField(max_length=50)
+    adress = models.OneToOneField(Adress, null=True, on_delete = models.SET_NULL)
+
+
 class Contact(models.Model):
     email = models.EmailField()
-    phone = models.IntegerField()
+    phone = models.CharField(null=True, max_length=30)
     instagram = models.CharField(null=True, max_length=30)
+    brecho = models.ForeignKey(Brecho, on_delete = models.CASCADE)
 
-    def __str__(self) -> str:
-        return self.email
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Produto(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    brecho = models.ForeignKey(Brecho, on_delete = models.CASCADE) 
+    tags = models.ManyToManyField(Tag)
+    
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    adress = models.ForeignKey(Adress, null=True, on_delete = models.SET_NULL)
+    email = models.EmailField()
 
 
 # Create your models here.
