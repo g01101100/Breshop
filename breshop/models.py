bd92db94eeddfdc555e22f96c13c89e594ae9171
Guilherme Lira <guilherme.liratsouza@gmail.com>
2025-11-05 19:53:07 -0300

    feat: criando str para os modelos

--- breshop/models.py
+++ breshop/models.py
@@ -6,11 +6,17 @@ class Adress(models.Model):
     city = models.CharField(max_length=20)
     street = models.CharField(max_length=50)
     number = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'state: {self.state} | city: {self.city}'
 	
 
 class Brecho(models.Model):
     name = models.CharField(max_length=50)
     adress = models.OneToOneField(Adress, null=True, on_delete = models.SET_NULL)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Contact(models.Model):
@@ -19,9 +25,15 @@ class Contact(models.Model):
     instagram = models.CharField(null=True, max_length=30)
     brecho = models.ForeignKey(Brecho, on_delete = models.CASCADE)
 
+    def __str__(self) -> str:
+            return self.email
+
 
 class Tag(models.Model):
     name = models.CharField(primary_key=True)
+    
+    def __str__(self) -> str:
+        return self.name
 
 
 class Produto(models.Model):
@@ -30,11 +42,17 @@ class Produto(models.Model):
     brecho = models.ForeignKey(Brecho, on_delete = models.CASCADE) 
     tags = models.ManyToManyField(Tag)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class User(models.Model):
     name = models.CharField(max_length=50)
     adress = models.ForeignKey(Adress, null=True, on_delete = models.SET_NULL)
     email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.name
 
 
 # Create your models here.
