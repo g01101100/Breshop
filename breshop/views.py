5202a9651a14edcac5b52e2a38e0a8438c805321
Guilherme Lira <guilherme.liratsouza@gmail.com>
2025-11-06 20:44:41 -0300

    feat(view): criando GET na função produtoAPI

--- breshop/views.py
+++ breshop/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import *
+import json
 
 
-def cadastro_Produto():
-    pass
+def produtosAPI(request):
+    if request.method == 'GET':
+        listaProdutos = list(Produto.objects.all().values())
+        return JsonResponse(listaProdutos, safe=False)
 
-def alterar_Produto():
-    pass
 
-def excluir_Produto():
-    pass
+def cadastrar_Produto(request):
+    return render(request, "cadastrar.html")
+
+def alterar_Produto(request):
+    return render(request, "alterar.html")
+
+def excluir_Produto(request):
+    return render(request, "excluir.html")
+
+
 # Create your views here.
