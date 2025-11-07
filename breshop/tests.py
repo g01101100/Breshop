6b47270f23748fa1e4a691a0d41088487c049ce7
Guilherme Lira <guilherme.liratsouza@gmail.com>
2025-11-07 12:14:29 -0300

    feat(tests): criado teste para GET User com e sem Adress

--- breshop/tests.py
+++ breshop/tests.py
@@ -2,7 +2,7 @@ import json
 from django.test import TestCase, Client
 from .models import *
 
-class ProdutoTests(TestCase):
+class ProdutoTest(TestCase):
 
     def setUp(self):
         self.client = Client()
@@ -27,6 +27,47 @@ class ProdutoTests(TestCase):
         self.assertTrue(len(data) > 0)
         self.assertEqual(data[0]["name"], 'camisa_teste')   
 
+class UserTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+       
+        adress = {
+            "CEP": "09999888",
+            "state": "Para",
+            "city": "Belem",
+            "street": "rua das orquideas",
+            "number": 1,
+        }
+        self.adress = Adress.objects.create(**adress)
+       
+        user01 = {
+            "name": "Joao",
+            "email": "joao@gmail.com"
+        }
+        user02 = {
+            "name": "Carol",
+            "email": "carol@gmail.com",
+            "adress": self.adress,
+        }
+        User.objects.create(**user01)
+        User.objects.create(**user02)
+        
+
+    def test_GET_User(self):
+        response = self.client.get('/user/')
+        self.assertEqual(response.status_code, 200)
+
+        listUser = response.json()
+
+        self.assertTrue(len(listUser) == 2)
+
+        self.assertEqual(listUser[0]["name"], 'Joao')
+        self.assertEqual(listUser[1]["email"], 'carol@gmail.com')
+        
+        adress_id = listUser[1]["adress"]
+        adress = Adress.objects.get(id=adress_id)
+        self.assertEqual(adress.city, 'Belem')
 
 
 
