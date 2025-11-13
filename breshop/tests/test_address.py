6a4023309671fab60b06f1411d9e37527acd1af2
Guilherme Lira <guilherme.liratsouza@gmail.com>
2025-11-13 19:03:37 -0300

    feat: criado teste e função post para classe AddressView

--- breshop/tests/test_address.py
+++ breshop/tests/test_address.py
@@ -46,4 +46,116 @@ class AddressTest(TestCase):
         self.assertEqual(addressList[0]["CEP"], '09999888')
 
 
+    def test_post_addess_url_returns_201(self):
+        response = self.client.post('/addresses/', data=json.dumps(self.address02), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
     
+    
+    def test_post_address_missing_cep(self):
+        response = self.client.post('/addresses/', data=json.dumps({
+                "state": "Bahia",
+                "city": "Salvador",
+                "street": "rua das flores",
+                "number": 43,
+            }), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+    
+    def test_post_address_whith_spaces_cep(self):
+        response = self.client.post('/addresses/', data=json.dumps({
+                "CEP": "        ",
+                "state": "Bahia",
+                "city": "Salvador",
+                "street": "rua das flores",
+                "number": 43,
+            }), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_post_address_invalid_cep_length(self):
+        response = self.client.post('/addresses/', data=json.dumps({
+                "CEP": "1234567",
+                "state": "Bahia",
+                "city": "Salvador",
+                "street": "rua das flores",
+                "number": 43,
+            }), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_if_post_address_with_special_common_character_in_cep_returns_201(self):
+        response = self.client.post('/addresses/', data=json.dumps({
+                "CEP": "12345-678",
+                "state": "Bahia",
+                "city": "Salvador",
+                "street": "rua das flores",
+                "number": 43,
+            }), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+    
+    
+    def test_post_address_missing_state(self):
+        response = self.client.post('/addresses/', data=json.dumps({
+                "CEP": "09999888",
+                "city": "Salvador",
+                "street": "rua das flores",
+                "number": 43,
+            }), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+    
+    def test_post_address_whith_spaces_state(self):
+        response = self.client.post('/addresses/', data=json.dumps({
+                "CEP": "09999888",
+                "state": "    ",
+                "city": "Salvador",
+                "street": "rua das flores",
+                "number": 43,
+            }), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+    
+    
+    def test_post_address_missing_city(self):
+        response = self.client.post('/addresses/', data=json.dumps({
+                "CEP": "09999888",
+                "state": "Bahia",
+                "street": "rua das flores",
+                "number": 43,
+            }), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+    
+    def test_post_address_whith_spaces_city(self):
+        response = self.client.post('/addresses/', data=json.dumps({
+                "CEP": "09999888",
+                "state": "Bahia",
+                "city": "   ",
+                "street": "rua das flores",
+                "number": 43,
+            }), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    
+    def test_post_address_missing_street(self):
+        response = self.client.post('/addresses/', data=json.dumps({
+                "CEP": "09999888",
+                "state": "Bahia",
+                "city": "salvador",
+                "number": 43,
+            }), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+    
+    def test_post_address_whith_spaces_street(self):
+        response = self.client.post('/addresses/', data=json.dumps({
+                "CEP": "09999888",
+                "state": "Bahia",
+                "city": "salvador",
+                "street": "    ",
+                "number": 43,
+            }), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+    
+    
+    def test_post_address_missing_number(self):
+        response = self.client.post('/addresses/', data=json.dumps({
+                "CEP": "09999888",
+                "state": "Bahia",
+                "city": "salvador",
+                "street": "rua das flores",
+            }), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
--- breshop/views/view_address.py
+++ breshop/views/view_address.py
@@ -6,7 +6,77 @@ import json
 
 class AddressView(View):
 
-    def get(self, request, *args, **kwargs):
-        listaAdress = list(Address.objects.all().values())
+    def get(self, *args, **kwargs):
+        listOfAdress = list(Address.objects.all().values())
+    
+        return JsonResponse(listOfAdress, safe=False)
+    
+    def post(self, request, *args, **kwargs):
+        listOfAddress = list(Address.objects.all().values())
+
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'JSON invalid'}, status=400)
         
-        return JsonResponse(listaAdress, safe=False)
+        cep = data.get('CEP')
+        state = data.get('state')
+        city = data.get('city')
+        street = data.get('street')
+        number = data.get('number')
+        
+        listOfFields = [cep, state, city, street, number]
+        
+
+        for field in listOfFields:
+            typeField = type(field)
+            if typeField == str:
+                field = field.strip()
+                if not field:
+                    return JsonResponse({'error': 'find some not null field'}, status=400)
+            elif typeField != int:
+                return JsonResponse({'error': 'find some not null field'}, status=400)
+        
+        cepCharacters = list(cep)
+
+        if len(cepCharacters) == 9 and cepCharacters[5] == '-':
+            cepCharacters.pop(5)
+            cep = ''.join(cepCharacters)
+
+        if len(cep.strip('')) != 8:
+            return JsonResponse({'error': 'Invalid field: CEP'}, status=400)
+
+
+        if type(number) != int:
+            return JsonResponse({'error': "number invalid type"}, status=400)
+        
+        state = state.capitalize()
+        city = city.capitalize()
+        
+        address = {
+            'CEP': cep,
+            'state': state,
+            'city': city,
+            'street': street,
+            'number': number,
+        }
+
+        if address in listOfAddress:
+            return JsonResponse({
+                'CEP': response.CEP,
+                'state': response.state,    
+                'city': response.city,    
+                'street': response.street,    
+                'number': response.number,    
+            }, status = 201)    
+
+        response = Address.objects.create(**address)
+
+        return JsonResponse({
+            'CEP': response.CEP,
+            'state': response.state,    
+            'city': response.city,    
+            'street': response.street,    
+            'number': response.number,    
+        }, status = 201)
+        
